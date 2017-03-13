72939fa99d4036c7c4a763a32e8da3875a698636
Konstantinos Andreadakis <andreadakis_k@DataCommunicationSA583.onmicrosoft.com>
2017-03-13 11:29:34 +0200

    yy

--- DCHotelManagerCore.UINew/Controllers/HomeController.cs
+++ DCHotelManagerCore.UINew/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace DCHotelManagerCore.UINew.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Your contact page";
 
             return View();
         }
